4d6487da87465202707b3e5f4fd970596b65afce
Marcus Lindblom <marcus@meridium.se>
2020-03-30 19:31:01 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh -l
 
-# output=$(dotnet application-insights-alerts.dll $1 $2 "$3" $4)
+output=$(dotnet application-insights-alerts.dll $1 $2 "$3" $4)
 
-# echo "::set-output name=time::$output"
+echo "::set-output name=time::$output"
 
-ls
-
-echo "Hello $1 $2 $3 $4"
-time=$(date)
-echo "::set-output name=time::$time"
+#echo "Hello $1 $2 $3 $4"
+#time=$(date)
+#echo "::set-output name=time::$time"
