1a605f62685e19304255f0760f52141f4cc83cc1
Marcus Lindblom <marcus@meridium.se>
2020-03-31 13:44:28 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/sh -l
 
-output=`dotnet /app/application-insights-alerts.dll $1 $2 "$3" $4`
+# output=`dotnet /app/application-insights-alerts.dll $1 $2 "$3" $4`
 
-# output=`cat test.json`
+output=`cat test.json`
 
+#count=`echo $output | jq '. | length'`
+
+count=`echo $output | jq '. | length'`
 timestamp=`echo $output | jq -r '.[0] .timeStamp'`
 exceptionType=`echo $output | jq -r '.[0] .exceptionType'`
 method=`echo $output | jq -r '.[0] .method'`
 message=`echo $output | jq -r '.[0] .message'`
 assembly=`echo $output | jq -r '.[0] .assembly'`
 
-printf "\n* A new exception has been detected in dev app:\n\n"
+printf "\n* Since last run we have detected $count new exceptions:\n\n"
 printf "|  Timestamp:             %s\t\n" "$timestamp"
 printf "|  ExceptionType:         %s\t\n" "$exceptionType"
 printf "|  Method:                %s\t\n" "$method"
@@ -18,6 +21,7 @@ printf "|  Message:               %s\t\n" "$message"
 printf "|  Assembly:              %s\t\n" "$assembly"
 printf "\n"
 
+echo "::set-output name=count::$count"
 echo "::set-output name=timestamp::$timestamp"
 echo "::set-output name=exceptionType::$exceptionType"
 echo "::set-output name=method::$method"
